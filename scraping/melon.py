774199cb7cb4f93c92116389d854ccc3529e23d4
Hyejin Eom <yazbyz@gmail.com>
2021-10-25 19:44:21 +0900

    [UPDATE] 멜론차트 노래 제목과 가수명

--- scraping/melon.py
+++ scraping/melon.py
@@ -3,7 +3,12 @@ from selenium import webdriver
 driver = webdriver.Chrome('/usr/local/bin/chromedriver') # 크롬을 통해서 스크래핑을 진행 크롬 드라이버 로딩
 driver.get('https://www.melon.com/chart/index.htm') # 멜론 차트 페이지 열기
 
-title = driver.find_element_by_xpath(
-    '//*[@id="frm"]/div/table'
+table = driver.find_element_by_xpath(
+    '//*[@id="frm"]/div/table/tbody'
 )
-print(title.text)
+
+tableRow = table.find_elements_by_tag_name('tr')
+
+for index, row in enumerate(tableRow):
+    songTitle = row.find_elements_by_tag_name('td')[5]
+    print(songTitle.text)
